0a00fe34fed56d064beaade9f54cb22535f64c85
Ildar Bikmamatov <ildar@bayrell.org>
2022-04-06 12:38:17 +0000

    update to 0.4 version

--- cloud_os.sh
+++ cloud_os.sh
@@ -12,18 +12,20 @@ TAG=`date '+%Y%m%d_%H%M%S'`
 function generate {
 	
 	ENV_PATH=$SCRIPT_PATH/example/env.conf
-	CLOUD_KEY_PATH=$SCRIPT_PATH/example/cloud.key
 	
 	if [ ! -f $ENV_PATH ]; then
 		
 		CLOUD_KEY=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 128`
-		CLOUD_MYSQL_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c 16`
+		SSH_PASSWORD=`cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#%^&*_\-=+~' | head -c 16`
+		
+		echo "Enter ssh username for Cloud OS:"
+        read SSH_USER
 		
 		cat $SCRIPT_PATH/example/env.example > $ENV_PATH
 		
-		sed -i "s|MYSQL_ROOT_PASSWORD=.*|MYSQL_ROOT_PASSWORD=${CLOUD_MYSQL_PASSWORD}|g" $ENV_PATH
-		sed -i "s|MYSQL_PASSWORD=.*|MYSQL_PASSWORD=${CLOUD_MYSQL_PASSWORD}|g" $ENV_PATH
 		sed -i "s|CLOUD_KEY=.*|CLOUD_KEY=${CLOUD_KEY}|g" $ENV_PATH
+		sed -i "s|SSH_USER=.*|SSH_USER=${SSH_USER}|g" $ENV_PATH
+		sed -i "s|SSH_PASSWORD=.*|SSH_PASSWORD=${SSH_PASSWORD}|g" $ENV_PATH
 
 		echo $CLOUD_KEY > $CLOUD_KEY_PATH
 		
@@ -31,22 +33,22 @@ function generate {
 	
 }
 
+function output {
+	. $SCRIPT_PATH/example/env.conf
+	echo "SSH_USER=${SSH_USER}"
+	echo "SSH_PASSWORD=${SSH_PASSWORD}"
+}
+
 
 case "$1" in
 	
 	download)
-		docker pull bayrell/alpine_mariadb:10.5-2
-		docker pull rabbitmq:3.8.9-management
-		docker pull bayrell/alpine_php_fpm:7.3-4
-		docker pull bayrell/bus_gateway:0.3.0
-		docker pull bayrell/cloud_os_standard:0.3.0
-		docker pull bayrell/load_balancer_http:0.3.0
+		docker pull bayrell/cloud_os_standard:0.4.0
 	;;
 	
 	create_network)
-		docker network create --subnet 172.21.0.1/16 --driver=overlay --attachable cloud_frontend -o "com.docker.network.bridge.name"="cloud_frontend"
-		
-		docker network create --subnet 172.22.0.1/16 --driver=overlay --attachable cloud_backend -o "com.docker.network.bridge.name"="cloud_backend"
+		docker network create --subnet 172.21.0.1/16 --driver=overlay \
+			--attachable cloud_network -o "com.docker.network.bridge.name"="cloud_network"
 		
 		sleep 2
 		
@@ -58,8 +60,11 @@ case "$1" in
 	;;
 	
 	compose)
-		docker stack deploy -c example/database.yaml database --with-registry-auth
-		docker stack deploy -c example/cloud_os.yaml cloud_os --with-registry-auth
+		docker-compose -f example/cloud_os.yaml -p "cloud_os" up -d
+	;;
+	
+	output)
+		output
 	;;
 	
 	setup)
@@ -70,7 +75,7 @@ case "$1" in
 	;;
 	
 	*)
-		echo "Usage: $SCRIPT_EXEC setup"
+		echo "Usage: $SCRIPT_EXEC {setup|compose|output}"
 		RETVAL=1
 
 esac
